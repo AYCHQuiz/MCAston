518b6557cc6eb716c7380d02578444aeadab5c5e
Aaron Schumacher <ajschumacher@gmail.com>
2013-01-20 01:03:02 -0500

    start to suggest using mongodb

--- web.js
+++ web.js
@@ -1,9 +1,23 @@
 var express = require('express');
 
+var mongo = require('mongodb');
+
+var mongoUri = process.env.MONGOLAB_URI || 
+  process.env.MONGOHQ_URL || 
+    'mongodb://localhost/mydb'; 
+
+//mongo.Db.connect(mongoUri, function (err, db) {
+//	db.collection('mydocs', function(er, collection) {
+//		collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
+//		    });
+//	    });
+//    });
+
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-	response.send('Hello Heroku!');
+	response.send('Hello Heroku!\n' + mongoUri);
     });
 
 var port = process.env.PORT || 5000;
