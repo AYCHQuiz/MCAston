d6e05106fda45645460aa121bb630c3fd086ea50
Aaron Schumacher <ajschumacher@gmail.com>
2013-01-26 16:47:01 -0500

    silly change to test new key

--- web.js
+++ web.js
@@ -17,7 +17,7 @@ var mongoUri = process.env.MONGOLAB_URI ||
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-	response.send('Hello Heroku!\n');
+	response.send('Hello World!\n');
     });
 
 var port = process.env.PORT || 5000;
