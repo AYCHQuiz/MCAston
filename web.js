bb9aba9d407dfb78d0c25d04b60a4cdeba8ddebe
Aaron Schumacher <ajschumacher@gmail.com>
2013-02-02 20:27:10 -0500

    preliminary method for connecting to db

--- web.js
+++ web.js
@@ -1,24 +1,21 @@
 var express = require('express');
-
-var mongo = require('mongodb');
-
-var mongoUri = process.env.MONGOLAB_URI || 
-  process.env.MONGOHQ_URL || 
-    'mongodb://localhost/mydb'; 
-
-//mongo.Db.connect(mongoUri, function (err, db) {
-//	db.collection('mydocs', function(er, collection) {
-//		collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
-//		    });
-//	    });
-//    });
-
-
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-	response.send('Hello Peoples!\n');
+var MongoClient = require('mongodb').MongoClient;
+var mongoURI = process.env.MONGOLAB_URI || 'mongodb://localhost/test';
+MongoClient.connect(mongoURI, function(err, db) {
+    console.log(mongoURI);
+    db.collectionNames(function(err, collections) {
+	console.log(collections);
+      });
+    app.get('/', function(request, response) {
+    db.collection("mcas").findOne(function(err, result) {
+	response.send(result);
+      });
     });
+  });
+
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
