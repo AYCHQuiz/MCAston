be2471070d596202e3d5052701f40b9be587cc68
Aaron Schumacher <ajschumacher@gmail.com>
2013-02-03 21:32:52 -0500

    switch from base mongodb to very nice mongojs

--- web.js
+++ web.js
@@ -1,23 +1,17 @@
 var express = require('express');
 var app = express.createServer(express.logger());
 
-var MongoClient = require('mongodb').MongoClient;
 var mongoURI = process.env.MONGOLAB_URI || 'mongodb://localhost/test';
-MongoClient.connect(mongoURI, function(err, db) {
-    console.log(mongoURI);
-    db.collectionNames(function(err, collections) {
-	console.log(collections);
-      });
-    app.get('/', function(request, response) {
-    db.collection("mcas").findOne(function(err, result) {
-	response.send(result);
-      });
-    });
+var collections = ['mcas'];
+var db = require('mongojs').connect(mongoURI, collections);
+
+app.get('/', function(request, response) {
+  db.mcas.find().limit(1, function(err, doc) {
+    response.send(doc);
   });
-
-
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-	console.log("Listening on " + port);
-    });
+  console.log("Listening on " + port);
+});
