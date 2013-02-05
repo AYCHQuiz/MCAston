0d69d8365b0bd6ed57f9645023592c47485a41bc
Aaron Schumacher <ajschumacher@gmail.com>
2013-02-04 21:16:20 -0500

    hacky un-templated display of image

--- web.js
+++ web.js
@@ -1,18 +1,20 @@
 var express = require('express');
 var app = express();
 
-var mongoURI = process.env.MONGOLAB_URI || 'mongodb://localhost/test';
+var mongo_url = process.env.MONGOLAB_URI || 'mongodb://localhost/test';
 var collections = ['mcas'];
-var db = require('mongojs').connect(mongoURI, collections);
+var db = require('mongojs').connect(mongo_url, collections);
 
-app.get('/', function(request, response) {
-  n = Math.floor(Math.random()*1236)
-  db.mcas.find().skip(n).limit(1, function(err, doc) {
-    response.send(doc);
+app.get('/', function(request, response){
+  n = Math.floor(Math.random()*1236);
+  db.mcas.find().skip(n).limit(1, function(err, docs){
+    response.send("<center><img src='http://i.imgur.com/" +
+                  docs[0].imgur +
+                  ".png' /></center>");
   });
 });
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
+app.listen(port, function(){
   console.log("Listening on " + port);
 });
