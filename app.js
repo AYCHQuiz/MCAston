3baf2ca70de754ad27f2d113a803b529bc0ef1ed
Aaron Schumacher <ajschumacher@gmail.com>
2013-02-07 10:23:37 -0500

    add jade templating

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 var express = require('express');
+
 var app = express();
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+});
 
 var mongo_url = process.env.MONGOLAB_URI || 'mongodb://localhost/test';
 var collections = ['mcas'];
@@ -8,9 +14,7 @@ var db = require('mongojs').connect(mongo_url, collections);
 app.get('/', function(request, response){
   n = Math.floor(Math.random()*1236);
   db.mcas.find().skip(n).limit(1, function(err, docs){
-    response.send("<center><img src='http://i.imgur.com/" +
-                  docs[0].imgur +
-                  ".png' /></center>");
+    response.render('index', {imgur: docs[0].imgur});
   });
 });
 
