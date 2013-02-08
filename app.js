c76b5b8eca31c3ea667d2a6b159582e896780e67
Aaron Schumacher <ajschumacher@gmail.com>
2013-02-08 17:12:13 -0500

    very first start at doing session stuff

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ app.configure(function(){
   app.set('port', process.env.PORT || 5000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.use(express.cookieParser(app.get('secret')));
+  app.use(express.session());
   app.use(express.static(__dirname + '/public'));
 });
 
@@ -16,6 +18,8 @@ var db = require('mongojs').connect(app.get('secret'), app.get('collections'));
 app.get('/', function(request, response){
   n = Math.floor(Math.random()*1236);
   db.mcas.find().skip(n).limit(1, function(err, docs){
+    request.session.imgur = docs[0].imgur;
+    console.log(request.session);
     response.render('index', {imgur: docs[0].imgur});
   });
 });
