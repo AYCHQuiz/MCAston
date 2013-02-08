65669284c8df2c881c49aca8f82a656008735f37
Aaron Schumacher <ajschumacher@gmail.com>
2013-02-07 23:24:06 -0500

    put port in same app.set convention

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ var express = require('express');
 var app = express();
 
 app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
   app.set('secret', process.env.MONGOLAB_URI || 'mongodb://localhost/test');
   app.set('collections', ['mcas']);
+  app.set('port', process.env.PORT || 5000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
   app.use(express.static(__dirname + '/public'));
 });
 
-//var collections = ['mcas'];
 var db = require('mongojs').connect(app.get('secret'), app.get('collections'));
 
 app.get('/', function(request, response){
@@ -20,7 +20,6 @@ app.get('/', function(request, response){
   });
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function(){
-  console.log("Listening on " + port);
+app.listen(app.get('port'), function(){
+  console.log("Listening on " + app.get('port'));
 });
