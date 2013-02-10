ee74f7f6336682085bb55956604f5d3a7da8a471
Aaron Schumacher <ajschumacher@gmail.com>
2013-02-09 19:02:57 -0500

    stick in 1kb limit on incoming data

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.configure(function(){
   app.set('port', process.env.PORT || 5000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.use(express.limit('1kb'));
   app.use(express.bodyParser());
   app.use(express.cookieParser(app.get('secret')));
   app.use(express.session());
